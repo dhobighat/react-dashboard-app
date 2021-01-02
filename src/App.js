24f448b25f84ef8c587d5b15c40fa96977547972
sandy007sandipan <$Passw0rd$>
2021-01-02 12:09:20 +0530

    Changing api call to load balancer

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
 
 
   componentDidMount() {
-    axios.get('http://aws-dev-alb-1190596654.us-east-1.elb.amazonaws.com/getMessage')
+    axios.get('/getMessage')
       .then(response => {
         this.setState({'status': response.data});
       })
