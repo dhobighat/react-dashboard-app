c90630f24824e5e97aa3fa0221fde367fb092342
sandy007sandipan <$Passw0rd$>
2021-01-10 13:27:49 +0530

    Changing api call to load balancer

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+S3_BUCKET="aws-dev-dashboard"
+echo "Deploying to the $S3_BUCKET bucket"
+
+# Install the AWS CLI so we can publish to S3
+pip install awscli --upgrade --user
+
+# Sync our build folder with our S3 bucket
+# --acl public-read says deploy the files with public read access
+# --delete says to delete files in the bucket that aren't present in the build folder
+#   this ensures that old assets built with webpack with hashed names get deleted
+#   when a new build of the app is made and the assets get new hash names
+aws s3 sync public/ "s3://$S3_BUCKET" --acl public-read --delete
--- src/App.js
+++ src/App.js
@@ -6,23 +6,33 @@ class App extends React.Component {
     super(props);
  
     this.state = {
-      status: "Getting current Status" 
+      dashboard-status: "Getting current Status"
+      product-status: "Getting current Status"
     }
   } 
 
 
   componentDidMount() {
-    axios.get('http://aws-dev-alb-1190596654.us-east-1.elb.amazonaws.com/getMessage')
+    axios.get('http://aws-dev-alb-1190596654.us-east-1.elb.amazonaws.com:8900/dashboard/getMessage')
       .then(response => {
-        this.setState({'status': response.data});
+        this.setState({'dashboard-status': response.data});
       })
       .catch(error => {
-        this.setState({'status': error});
+        this.setState({'dashboard-status': error});
       });
+
+      axios.get('http://aws-dev-alb-1190596654.us-east-1.elb.amazonaws.com:8901/product/getMessage')
+         .then(response => {
+           this.setState({'product-status': response.data});
+         })
+         .catch(error => {
+           this.setState({'product-status': error});
+         });
     }
 
   render() {
-    return <h1>{this.state.status} </h1>;
+    return <h1>Response from Dashboard Service : {this.state.dashboard-status} </h1>;
+    return <h1>Response from Product Service :{this.state.product-status} </h1>;
   }
 }
 
