7d2f9c456f0eb3ca2014f3c271d5028110171f06
sandy007sandipan <$Passw0rd$>
2021-01-10 14:31:03 +0530

    Changing api call to load balancer

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
