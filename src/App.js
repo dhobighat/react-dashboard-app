7459da7ae814176fbb9abe3aa7231ba9cfed12ff
sandy007sandipan <$Passw0rd$>
2021-01-24 18:24:23 +0530

    Auth0 Integration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import { useAuth0 } from '@auth0/auth0-react';
 import AppHeader from './views/component/header/AppHeader';
 import UserDashboard from './views/component/dashboard/UserDashboard';
